5e35d960648d5f8e20ff3b35a50505f1469ca91e
BL-MariaSelvam <maria.selvam@bridgelabz.com>
2026-01-05 16:37:31 +0530

    3 Valid Email Added Regex

---
+++ BridgeLabzTasks/src/Day19/UC3.java
@@ -0,0 +1,26 @@
+package Day19;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC3 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Email");
+		String email=sc.next();
+		String pattern="^[a-z]+([\\.a-z]+)?@[a-z]+\\.[a-z]+([\\.a-z]{3}+)?$";
+		Pattern p=Pattern.compile(pattern);
+		Matcher m=p.matcher(email);
+		
+		if(m.find()) {
+			System.out.println("Entered email is valid");
+		}
+		else {
+			System.out.println("Entered email is not valid");
+		}
+
+	}
+
+}
