4f2ab99bb5b02da308fc23381c75da0afdc34d87
BL-MariaSelvam <maria.selvam@bridgelabz.com>
2026-01-19 11:39:25 +0530

    Added Day27

---
+++ BridgeLabzTasks/src/Day27/AddressBook.java
@@ -0,0 +1,155 @@
+package Day27;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AddressBook {
+	
+	
+	private List<Contact> contacts=new ArrayList<>();
+	
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+	public void addContact(Contact contact) {
+		
+		boolean duplicates=contacts.stream().anyMatch(existing->existing.equals(contact));
+		if(duplicates) {
+			System.out.println("Duplicate Entry not allowed");
+		}
+			
+		else{
+			contacts.add(contact);
+			System.out.println("Contact Details added");
+		}
+		
+	}
+	
+	public void displayContacts() {
+		
+		for(Contact contact:contacts) {
+			System.out.println("firstName"+contact.getFirstName());
+			System.out.println("lastName"+contact.getLastName());
+			System.out.println("Address"+contact.getAddress());
+			System.out.println("City"+contact.getCity());
+			System.out.println("State"+contact.getState());
+			System.out.println("ZipCode"+contact.getZipCode());
+			System.out.println("Mobile"+contact.getMobile());
+			System.out.println("Email"+contact.getEmail());
+		}
+	}
+
+	public void editContact(String fName, Contact updatedContact) {
+		boolean found=false;
+		for(Contact contact:contacts) {
+			if(fName.equals(contact.getFirstName())){
+				contact.setFirstName(updatedContact.getFirstName());
+				contact.setLastName(updatedContact.getLastName());
+				contact.setAddress(updatedContact.getAddress());
+				contact.setCity(updatedContact.getCity());
+				contact.setState(updatedContact.getState());
+				contact.setZipCode(updatedContact.getZipCode());
+				contact.setMobile(updatedContact.getMobile());
+				contact.setEmail(updatedContact.getEmail());
+				found=true;
+				break;
+			}
+		}
+		if(found==true)
+		{
+			System.out.println("Contact Details updated");
+		}
+		else {
+			System.out.println("Record not found");
+		}
+	}
+
+	public void deleteContact(String firName) {
+		Iterator<Contact> contact=contacts.iterator();
+		boolean flag=false;
+		
+		while(contact.hasNext()) {
+			if(contact.next().getFirstName().equalsIgnoreCase(firName)) {
+				contact.remove();
+				flag=true;
+			}
+			else {
+				
+				flag=false;
+			}
+		}
+		if(flag==true)
+		System.out.println("Contact removed");
+		else
+			System.out.println("Record not found");
+		
+	}
+	
+	
+	public void displayContactsSortedByName() {
+
+	    List<Contact> sortedContacts = contacts.stream()
+	            .sorted(Comparator.comparing(Contact::getFirstName,
+	                    String.CASE_INSENSITIVE_ORDER))
+	            .collect(Collectors.toList());
+
+	    if (sortedContacts.isEmpty()) {
+	        System.out.println("No contacts available");
+	        return;
+	    }
+
+	    System.out.println("Contacts Sorted Alphabetically by First Name:");
+	    sortedContacts.forEach(System.out::println);
+	}
+	
+	public void displayContactsSortedByCity() {
+
+	    List<Contact> sortedContacts = contacts.stream()
+	            .sorted(Comparator.comparing(Contact::getCity,
+	                    String.CASE_INSENSITIVE_ORDER))
+	            .collect(Collectors.toList());
+
+	    if (sortedContacts.isEmpty()) {
+	        System.out.println("No contacts available");
+	        return;
+	    }
+
+	    System.out.println("Contacts Sorted Alphabetically by City:");
+	    sortedContacts.forEach(System.out::println);
+	}
+	
+	public void displayContactsSortedByState(){
+
+	    List<Contact> sortedContacts = contacts.stream()
+	            .sorted(Comparator.comparing(Contact::getState,
+	                    String.CASE_INSENSITIVE_ORDER))
+	            .collect(Collectors.toList());
+
+	    if (sortedContacts.isEmpty()) {
+	        System.out.println("No contacts available");
+	        return;
+	    }
+
+	    System.out.println("Contacts Sorted Alphabetically by State:");
+	    sortedContacts.forEach(System.out::println);
+	}
+	
+	public void displayContactsSortedByZipCode() {
+
+	    List<Contact> sortedContacts = contacts.stream()
+	            .sorted(Comparator.comparing(Contact::getZipCode))
+	            .collect(Collectors.toList());
+
+	    if (sortedContacts.isEmpty()) {
+	        System.out.println("No contacts available");
+	        return;
+	    }
+
+	    System.out.println("Contacts Sorted Alphabetically by ZipCode:");
+	    sortedContacts.forEach(System.out::println);
+	}
+		
+	}
---
+++ BridgeLabzTasks/src/Day27/StreamAPI.java
@@ -0,0 +1,61 @@
+package Day27;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StreamAPI {
+
+	public static void main(String[] args) {
+		List<Integer> list=new ArrayList<Integer>();
+		
+		for(int i=0;i<5;i++) {
+			list.add(i);
+		}
+		Iterator<Integer> iter=list.iterator();
+		
+		while(iter.hasNext()) {
+			Integer i=iter.next();
+			System.out.println(i);
+		}
+		
+		class MyConsumer implements Consumer<Integer>{
+
+			@Override
+			public void accept(Integer t) {
+				System.out.println(t);	
+			}
+	}
+		MyConsumer action=new MyConsumer();
+		list.forEach(action);
+		
+		list.forEach(new MyConsumer() {
+			@Override
+			public void accept(Integer t) {
+				System.out.println(t);	
+			}
+		});
+		
+		Consumer<Integer> myaction=n->System.out.println(n);
+		list.forEach(myaction);
+		
+		list.forEach(n->System.out.println(n));
+		
+		Function<Integer, Double> toDoubleValue=Integer::doubleValue;
+		list.forEach(n->System.out.println(toDoubleValue.apply(n)));
+		
+		Predicate<Integer> isEvenFunction=n->n>0 && n%2==0;
+		list.forEach(n->System.out.println("The number "+n+ "is to test even "+isEvenFunction.test(n)));
+		
+		List<Double> doubleList=list.stream()
+									.filter(isEvenFunction)
+									.map(toDoubleValue)
+									.collect(Collectors.toList());
+		System.out.println(doubleList);
+
+}
+}
